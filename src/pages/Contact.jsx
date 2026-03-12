8f6ad7939da39f8bd34a291af23a27c3559a21c7
Manish Kumar <manishkumar426001@email.com>
2026-03-12 13:40:06 +0530

    Practice auto deploy update on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact() {
     return (
       <section className="page-card">
         <h1>Contact</h1>
-        <p>Feel free to reach out for collaboration, internships, or project discussions.</p>
+        <p>Feel free to reach out for collaboration, internships, or project discussions or Devops opportunities.</p>
   
         <form className="contact-form">
           <input type="text" placeholder="Enter your name" />
